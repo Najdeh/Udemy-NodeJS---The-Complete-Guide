0dd7f324431da05774d77812a11ce58024375c93
Miro <m.najdeh@gmail.com>
2019-12-03 13:00:30 +0100

    Section 6: Working with Dynamic Content

--- express/app.js
+++ express/app.js
@@ -1,8 +1,11 @@
 const http = require('http');
+const path = require('path');
 
 const express = require('express');
 
-const bodyParser = require('body-parser')
+const rootDir = require('./util/path');
+
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -10,10 +13,15 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+});
 
 const server = http.createServer(app);
 
